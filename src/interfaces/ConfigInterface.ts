498d1311720a6ac1abab2124fea53a3a39e960da
CoutCout <72403468+ArthurCouturier@users.noreply.github.com>
2025-04-02 23:13:45 +0200

    feat: :sparkles: added TVA in account for computes

--- src/interfaces/ConfigInterface.ts
+++ src/interfaces/ConfigInterface.ts
@@ -5,9 +5,11 @@ export default interface ConfigInterface {
     uuid: string;
     name: string;
     tjm: AbstractNumberInterface;
+    inclTjmTVA: boolean;
     tax: TaxInterface[];
     workedDays: AbstractNumberInterface[];
     total: AbstractNumberInterface;
+    inclTotalTVA: boolean;
     createdAt: Date;
     updatedAt: Date;
 }
